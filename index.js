705e48e176a50a480a82264ac9c2ea458c020829
swadique <swadique@dexlock.com>
2021-02-09 00:52:15 +0530

    Basic routes setup completed

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const MongoClient = require("./db/MongoClient");
+const { SellerRoutes } = require("./routes/seller");
+const { PublicRoutes } = require("./routes/public");
+const { BuyerRoutes } = require("./routes/buyer");
+
 const app = express();
 const PORT = 8001;
 
@@ -11,7 +15,10 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.get("/", (req, res) => res.send("Express  Server"));
+app.use("/public", PublicRoutes);
+app.use("/buyer", BuyerRoutes);
+app.use("/seller", SellerRoutes);
+
 app.listen(PORT, () => {
   console.log(`\u26a1[server]: Server is running at http://localhost:${PORT}`);
 });
